c24fc119f14775cd152c9a64b61135b1fc477655
Alicia Weenum <weenum.alicia@gmail.com>
2020-10-20 17:48:06 -0400

    commit tree d0f76442092b2a4f5762821a15ec02747972d429

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown(number)
 
-def countdown
+    while number > 0
+
+        puts "#{number} SECOND(S)!"
+        number -=1
+    end
+
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(time)
+    while time > 0
+        time -=1
+        sleep 1
+    end
 end
