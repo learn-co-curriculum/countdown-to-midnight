45337c8b9db908de9aef883f9828c1f76b5ac686
Tinkergit <nicholas.stagno@gmail.com>
2021-02-02 17:44:26 -0500

    commit tree e787401db89d233589c29c2ac3a770cf41e6d0dc

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+        if number == 0
+            return "HAPPY NEW YEAR!"
+        end
+    end
 end
+
+countdown(10)
+
+def countdown_with_sleep(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+        sleep 1
+        if number == 0
+            return "HAPPY NEW YEAR!"
+        end
+    end
+end
+
+countdown_with_sleep(10)
